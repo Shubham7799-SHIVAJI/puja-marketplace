e237ead80ca6c16f45028527ed6bfc1dc85a17ad
Shubham7799-SHIVAJI <agarwal.77.99shubham@gmail.com>
2026-03-06 13:22:05 +0530

    HAR HAR MAHADEV

---
+++ Backend/puja-backend/src/main/java/com/SHIVA/puja/PujaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.SHIVA.puja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PujaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PujaBackendApplication.class, args);
+	}
+
+}
