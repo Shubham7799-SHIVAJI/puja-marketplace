ed91a281f6daf1093c476722d7bec04de6536ff2
Shubham Agarwal <agarwal.77.99shubham@gmail.com>
2026-03-06 22:53:56 +0530

    new frontend

--- Frontend/MAHAKAL-PUJA/src/app/app.spec.ts
+++ Frontend/MAHAKAL-PUJA/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SHIVA-FLOWER-AND-PUJA');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MAHAKAL-PUJA');
   });
 });
--- Frontend/MAHAKAL-PUJA/src/app/app.ts
+++ Frontend/MAHAKAL-PUJA/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('SHIVA-FLOWER-AND-PUJA');
+  protected readonly title = signal('MAHAKAL-PUJA');
 }
